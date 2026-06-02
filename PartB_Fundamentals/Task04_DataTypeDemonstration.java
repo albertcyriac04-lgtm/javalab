bf6e9af676524b47f5e981b36590002730e8cf66
albert <albertcyriac04@gmail.com>
2026-06-02 16:00:00 +0530

    Add Task04 DataTypeDemonstration

---
+++ PartB_Fundamentals/Task04_DataTypeDemonstration.java
@@ -0,0 +1,22 @@
+// Task 4: Data type demonstration
+class Demo
+{
+    public static void main(String args[])
+    {
+        byte by = 100;
+        short sh = 1000;
+        int i = 100;
+        long l = 100000L;
+        float f = 25.5f;
+        double d = 123.456;
+        char ch = 'A';
+        boolean b = true;
+
+        System.out.println("Integer value : " + i);
+        System.out.println("Long value    : " + l);
+        System.out.println("Float value   : " + f);
+        System.out.println("Double value  : " + d);
+        System.out.println("Character    : " + ch);
+        System.out.println("Boolean      : " + b);
+    }
+}
