7b28ade009793c2bc14249a17459ac9faeff0d3e
albert <albertcyriac04@gmail.com>
2026-06-11 10:00:00 +0530

    Add 01 Student Default Constructor

---
+++ PartA_OOP/01_Student_Default_Constructor.java
@@ -0,0 +1,16 @@
+// Program 1: Default constructor that displays a welcome message
+class Student
+{
+    Student()
+    {
+        System.out.println("Welcome to Student Class");
+    }
+}
+
+class Demo
+{
+    public static void main(String args[])
+    {
+        Student s = new Student();
+    }
+}
