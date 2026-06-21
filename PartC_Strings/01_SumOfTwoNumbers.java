438d90960986b158acd8e4ff3b24cd021d79cb30
albert <albertcyriac04@gmail.com>
2026-06-21 10:00:00 +0530

    Add 01 SumOfTwoNumbers

---
+++ PartC_Strings/01_SumOfTwoNumbers.java
@@ -0,0 +1,17 @@
+// Program 1: Sum of two numbers
+import java.util.Scanner;
+
+class Demo
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+        sc.close();
+    }
+}
